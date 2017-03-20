567fe0a714f3c13e0449f2f2885f4beb2d3c21a8
ghltshubh <shubhankargahlot@gmail.com>
2017-03-20 04:17:33 -0500

    Changed column name 'sales' to 'department'

--- HRAnalytics.R
+++ HRAnalytics.R
@@ -4,6 +4,7 @@ origData <- as.data.frame(gsheet2tbl('https://docs.google.com/spreadsheets/d/19-
 employeeID <- c(1:dim(origData)[1])
 HRData<- as.data.frame(cbind(employeeID, origData))
 colnames(HRData)[1] <- c('employee_ID')
+names(HRData)[names(HRData) == "sales"] <- "department"
 # LD: change salary from character to ordinal
 HRData$salary <- ordered(HRData$salary, levels=c("low", "medium", "high"))
 # AA: changed the remaining variables to their correct form & checked if no column is missed.
