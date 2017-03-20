5ed60de68e20bde1e590e8c8911166b7cab77877
Ankush Agrekar <a.agrekar@gmail.com>
2017-03-19 22:55:48 -0500

    added code to check the variable types

--- HRAnalytics.R
+++ HRAnalytics.R
@@ -6,3 +6,12 @@ HRData<- as.data.frame(cbind(employeeID, origData))
 colnames(HRData)[1] <- c('employee_ID')
 # LD: change salary from character to ordinal
 HRData$salary <- ordered(HRData$salary, levels=c("low", "medium", "high"))
+# AA: changed the remaining variables to their correct form & checked if no column is missed.
+HRData$sales <- as.factor(HRData$sales)
+HRData$Work_accident <- as.factor(HRData$Work_accident)
+HRData$left <- as.factor(HRData$left)
+HRData$promotion_last_5years <- as.factor(HRData$promotion_last_5years)
+
+factVars <- colnames(HRData)[sapply(HRData, is.factor)] 
+numVars  <- colnames(HRData)[sapply(HRData, is.numeric)]
+stopifnot(length(numVars) + length(factVars) == length(colnames(HRData)))
